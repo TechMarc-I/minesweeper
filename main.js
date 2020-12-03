bf1fe9a5fd14daa711e166b730455f848f88926d
Ian Marcelo <ianmarcelo02@gmail.com>
2020-12-02 20:18:01 -0500

    Created framework around grid and empty canvas for header information

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ let numColumns = 10;
 let boardContainer = document.querySelector("#board");
 let cellCount = numRows*numColumns;
 
+//Function to Create Grid 
 const generateGrid = function(){
   let cellId = 0;
 
   while (cellId < cellCount)
   {
-    cellId++;
+    cellId += 1;
     let cell = document.createElement('div');
     cell.className = "cell";
     cell.id = "cell-" + cellId;
